f6083379551cdbf892fea881a005e123e90eb3f9
“Rajeshwari <kotarajeswari003@gmail.com>
2023-07-14 07:23:37 +0530

    [Rajeshwari kota] Adding DataStructures

---
+++ DataStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....Data Structure....");
+            Console.ReadLine();
+        }
+    }
+}
